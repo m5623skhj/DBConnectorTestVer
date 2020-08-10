87797240788736e585a2be7a773f77442171b929
m5623skhj <m5623skhj@gmail.com>
2020-08-10 23:32:12 +0900

    DBConnector TestVer

--- DBConnector.cpp
+++ DBConnector.cpp
@@ -1,5 +1,5 @@
 #include "DBConnector.h"
-#pragma comment(lib, "libmySQL.lib")
+#include <iostream>
 
 DBConnector::DBConnector()
 {
@@ -13,27 +13,69 @@ DBConnector::~DBConnector()
 
 void DBConnector::ConnectDB()
 {
-	mysql_real_connect(con, "127.0.0.1",
-		"root", "1234", "testschema", 3306, NULL, 0);
+	_con = mysql_init(nullptr);
+	if (!mysql_real_connect(_con, "127.0.0.1",
+		"root", "1234", "testschema", 3306, NULL, 0))
+	{
+		std::cout << mysql_error(_con);
+	}
 }
 
 void DBConnector::DBSendQuery(std::string query)
 {
-	mysql_query(con, query.c_str());
+	if(mysql_query(_con, query.c_str()) != 0)
+	{
+		std::cout << mysql_error(_con);
+	}
 }
 
-MYSQL_ROW_OFFSET DBConnector::DBGetResult()
+void DBConnector::DBStoreResult()
 {
-	res = mysql_store_result(con);
-	return mysql_row_tell(res);
+	_res = mysql_store_result(_con);
+}
+
+MYSQL_ROWS* DBConnector::DBGetResult()
+{
+	return mysql_row_tell(_res);
+}
+
+DBConnector::ROW_ARRAY* DBConnector::GetDBArr()
+{
+	//res = mysql_store_result(con);
+	ROW_ARRAY* row = new ROW_ARRAY();
+	row->_row = mysql_row_tell(_res);
+	row->itemMap = &_rowMap;
+
+	return row;
+}
+
+void DBConnector::DBArray()
+{
+	MYSQL_FIELD* field = mysql_fetch_fields(_res);
+	int numOfFields = mysql_num_fields(_res);
+	for(int i=0; i<numOfFields; ++i)
+	{
+		_rowMap.insert({ field[i].name, i });
+	}
 }
 
 void DBConnector::FreeResult()
 {
+	mysql_free_result(_res);
+}
 
+void DBConnector::FreeResult(ROW_ARRAY* deleteRow)
+{
+	if (deleteRow != nullptr)
+	{
+		delete deleteRow;
+		deleteRow = nullptr;
+	}
+
+	mysql_free_result(_res);
 }
 
 void DBConnector::CloseDB()
 {
-
+	mysql_close(_con);
 }
--- DBConnector.h
+++ DBConnector.h
@@ -7,16 +7,39 @@
 class DBConnector
 {
 public :
+	struct ROW_ARRAY
+	{
+		char* operator[](std::string columnName)
+		{
+			auto findIter = itemMap->find(columnName);
+			if (findIter == itemMap->end())
+				return nullptr;
+
+			//return &_row[findIter->second];
+			return _row->data[findIter->second];
+		}
+
+		MYSQL_ROWS*		_row;
+		std::unordered_map<std::string, int>* itemMap;
+	};
+
 	DBConnector();
 	~DBConnector();
 
 	void ConnectDB();
 	void DBSendQuery(std::string query);
-	MYSQL_ROW_OFFSET DBGetResult();
+	void DBStoreResult();
+	MYSQL_ROWS* DBGetResult();
+	ROW_ARRAY* GetDBArr();
+
+	void DBArray();
+
 	void FreeResult();
+	void FreeResult(ROW_ARRAY* deleteRow);
 	void CloseDB();
 
 private :
-	MYSQL* con;
-	MYSQL_RES* res;
+	MYSQL*									_con;
+	MYSQL_RES*								_res;
+	std::unordered_map<std::string, int>	_rowMap;
 };
